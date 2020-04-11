e86aa0ba2d7c148e117b3131d175b6feb4364524
Greg Magolan <gmagolan@gmail.com>
2020-04-10 17:53:45 -0700

    a54a87ed fix(builtin): consider guard paths to be out paths in linker

--- @bazel_create/index.js
+++ @bazel_create/index.js
@@ -140,7 +140,7 @@ npm_install(
     package_lock_json = "//:package-lock.json",
 )`;
 
-  let workspaceContent = `# Bazel workspace created by @bazel/create 1.5.0-48-g0a29011
+  let workspaceContent = `# Bazel workspace created by @bazel/create 1.5.0-48-ga54a87e
 
 # Declares that this directory is the root of a Bazel workspace.
 # See https://docs.bazel.build/versions/master/build-ref.html#workspace
